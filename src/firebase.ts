cd40dd09f463ddb41e23d7cef269c7612b332f9b
Sujan <work.sujan26@gmail.com>
2025-03-30 13:11:34 +0530

    changed gitignore

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,8 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-import firebaseConfig from "./firebaseConfig"; // Adjust the path to your firebaseConfig file
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA6y-qwrJ_EZyeIv-toVV6xg72oDLVOCVQ",
+  authDomain: "country-capital-quiz-fb04b.firebaseapp.com",
+  projectId: "country-capital-quiz-fb04b",
+  storageBucket: "country-capital-quiz-fb04b.firebasestorage.app",
+  messagingSenderId: "642080487904",
+  appId: "1:642080487904:web:d24f635baf2b4f21561385",
+  measurementId: "G-ME66E6XBL7",
+};
 
 const app = initializeApp(firebaseConfig);
 const db: Firestore = getFirestore(app);
